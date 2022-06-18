40dd9408b22abbd90fbac17d160597184ce6bee1
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-06-18 18:44:44 -0300

    alterado script.js

---
+++ estilos/script.js
@@ -0,0 +1,22 @@
+var hora = new Date().getHours()
+var hora = 
+horario.innerHTML = `Agora são ${hora} horas`
+var img = window.document.getElementById('foto')
+
+if (hora >= 0 && hora < 6) {
+    img.src = 'imagens/noite.jpeg'
+    img.alt = 'foto da noite'
+    document.body.style.background = '#373f48'
+} else if (hora < 12) {
+    img.src = 'imagens/manha.jpeg'
+    img.alt = 'foto da manhã'
+    document.body.style.background = '#EDC259'
+} else if (hora < 18) {
+    img.src = 'imagens/tarde.jpeg'
+    img.alt =  'foto da tarde'
+    document.body.style.background = '#4DAD80'
+} else {
+    img.src = 'imagens/noite.jpeg'
+    img.alt = 'foto da noite'
+    document.body.style.background = '#373f48'
+}
