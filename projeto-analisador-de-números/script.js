94282041b14c80e36c1a637cfd1eb229f6c246c9
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-07-06 00:01:37 -0300

    alterado layout

---
+++ projeto-analisador-de-números/script.js
@@ -0,0 +1,33 @@
+var numero = document.getElementById('iptnumero')
+var lista = document.getElementById('valores')
+var lista_numeros = []
+
+function isNumber(num) {
+    if (Number(num) >= 1 && Number(num) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(lista, numero) {
+    if (lista.indexOf(Number(numero)) != -1) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function adicionar() {
+    if (numero.value.length == 0 || !isNumber(numero.value)) {
+        window.alert('Digite um valor entre 1 e 100')
+    } else if (!inLista(lista_numeros, numero.value)) {
+        window.alert('Este número já está na lista.')
+    } else {
+        lista_numeros.push(Number(numero.value))
+        window.alert('Número adicionado com sucesso.')
+        var item = document.createElement('option')
+        item.text = `Valor ${numero.value} adicionado`
+        lista.appendChild(item)
+    }
+}
